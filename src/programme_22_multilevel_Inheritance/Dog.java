dbca7bcb2d03b9d1b4c19e261a5c6f65cb925b3a
Gautamseema <seematest244@gmail.com>
2023-11-07 23:00:50 +0000

    javaweek4hw

---
+++ src/programme_22_multilevel_Inheritance/Dog.java
@@ -0,0 +1,8 @@
+package programme_22_multilevel_Inheritance;
+
+public class Dog extends Animal {
+    public void bark(){
+
+        System.out.println("barking...");
+    }
+}
---
+++ src/programme_23_method_overriding/Vehicle.java
@@ -0,0 +1,14 @@
+package programme_23_method_overriding;
+
+/**
+ * java Programme to illustrate the use of java method overriding
+ *  Creating a parent class
+ */
+
+public class  Vehicle {
+    //defining a method
+    public void run(){
+
+        System.out.println("Vehicle is running");
+    }
+}
